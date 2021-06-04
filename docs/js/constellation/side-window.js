c78a3a87c3edb8883a77dabeb842bfc33accb127
David Dieulivol <david.dieulivol@epfl.ch>
2021-06-04 13:30:49 +0200

    function to array functions where applicable

--- docs/js/constellation/side-window.js
+++ docs/js/constellation/side-window.js
@@ -2,13 +2,13 @@ function open_side_window(data) {
   document.getElementById("side_window").style.width = `${width / 3}px`;
   document.getElementById("side-title").textContent = `${toTitles(data.source.id)} x ${toTitles(data.target.id)}`;
 
+  // Our main dataset in the side window.
   const relationshipStats = data.title.map(function (title, i) {
     return [title, new Date(data.year[i], 0, 1), data.budget[i], data.revenue[i], data.imdb_rating[i]];
-  });
+  }).sort((a, b) => a[1] - b[1]);
 
-  relationshipStats.sort(function(a, b) { return a[1] - b[1] })
-  const groups = d3.map(relationshipStats, function(d) { return (d[0]) }).keys()
-  const subgroups = [2, 3]
+  const groups = d3.map(relationshipStats, (d) => (d[0])).keys();
+  const subgroups = [2, 3];
 
   // Add X axis
   const x = d3.scaleBand()
@@ -23,7 +23,7 @@ function open_side_window(data) {
 
   // Y axis
   const y = d3.scaleLinear()
-    .domain([0, d3.max(relationshipStats, function(d) { return Math.max(d[2], d[3]); })])
+    .domain([0, d3.max(relationshipStats, (d) => Math.max(d[2], d[3]))])
     .range([side_height, 0]);
   side_y.call(d3.axisLeft(y).tickFormat(d3.format("($.2s")));
 
@@ -43,7 +43,7 @@ function open_side_window(data) {
 
   side_data.selectAll(".bar-group").data(relationshipStats).exit().remove();
   side_data.selectAll(".bar-group").selectAll("rect")
-    .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+    .data((d) => subgroups.map((key) => { return {key: key, value: d[key]}; }))
     .exit().remove()
   
   side_data.selectAll(".bar-group")
@@ -51,31 +51,30 @@ function open_side_window(data) {
       .enter()
         .append("g").attr("class", "bar-group")
       .selectAll("rect")
-        .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+        .data((d) => subgroups.map((key) => { return {key: key, value: d[key]}; }))
         .enter().append("rect")
-        .on("mouseover", function(d) {		
+        .on("mouseover", (d) => {
           tooltip.transition()		
               .duration(200)		
               .style("opacity", .9);		
-              tooltip	.html(d3.format("($.2s")(d.value))	
+              tooltip.html(d3.format("($.2s")(d.value))	
               .style("left", (d3.event.pageX) + "px")		
               .style("top", (d3.event.pageY - 28) + "px");	
-          })					
-      .on("mouseout", function(d) {		
+          })
+      .on("mouseout", (d) =>
         tooltip.transition()		
               .duration(500)		
-              .style("opacity", 0);	
-      });
+              .style("opacity", 0));
 
   side_data.selectAll(".bar-group")
     .transition().duration(500)
-      .attr("transform", function(d) { return "translate(" + x(d[0]) + ",0)"; })
+      .attr("transform", (d) => "translate(" + x(d[0]) + ",0)")
       .selectAll("rect")
-        .attr("x", function(d) { return xSubgroup(d.key); })
-        .attr("y", function(d) { return y(d.value); })
+        .attr("x", (d) => xSubgroup(d.key))
+        .attr("y", (d) => y(d.value))
         .attr("width", xSubgroup.bandwidth())
-        .attr("height", function(d) { return side_height - y(d.value); })
-        .attr("fill", function(d) { return color(d.key); })
+        .attr("height", (d) => side_height - y(d.value))
+        .attr("fill", (d) => color(d.key))
         .attr("opacity", '0.8');
 
 
@@ -85,13 +84,13 @@ function open_side_window(data) {
       
   side_data.selectAll("text")
     .attr('class', 'place-label')
-    .attr("x", function(d) {return x(d[0]) + (x.bandwidth() / 2) + 10})
-    .attr("y", function(d) {return rating(d[4]) > side_height/2  ? rating(d[4]) - 10 : rating(d[4]) + 15 })
-    .text(function(d) {return d[4]})
+    .attr("x", (d) => x(d[0]) + (x.bandwidth() / 2) + 10)
+    .attr("y", (d) => rating(d[4]) > side_height/2  ? rating(d[4]) - 10 : rating(d[4]) + 15)
+    .text((d) => d[4])
     .attr("fill", "darkgoldenrod")
     .attr("font", "bold")
     .attr("font-size", "12px")
-    .style("text-anchor", function(d) { return x(d[1]) < side_width/2 ? "start" : "end"});
+    .style("text-anchor", (d) => x(d[1]) < side_width/2 ? "start" : "end");
 
   circles = side_data.selectAll("circle").data(relationshipStats);
   circles.exit().remove();
@@ -103,12 +102,12 @@ function open_side_window(data) {
   circles.transition()
     .duration(500)
     .attr("fill", "darkgoldenrod")
-    .attr("cx", function (d) { return x(d[0]) + (x.bandwidth() / 2) })
-    .attr("cy", function (d) { return rating(d[4]) })
+    .attr("cx", (d) => x(d[0]) + (x.bandwidth() / 2))
+    .attr("cy", (d) => rating(d[4]))
     .attr("r", 4);
 
-  side_data.selectAll("path").data([relationshipStats], function(d) { return d[4] }).exit().remove();
-  line = side_data.selectAll("path").data([relationshipStats], function(d) { return d[4] });
+  side_data.selectAll("path").data([relationshipStats], (d) => d[4]).exit().remove();
+  line = side_data.selectAll("path").data([relationshipStats], (d) => d[4]);
   line
     .enter()
     .append("path")
@@ -119,15 +118,15 @@ function open_side_window(data) {
     .attr("stroke", "darkgoldenrod")
     .attr("stroke-width", 2)
     .attr("d", d3.line()
-      .x(function(d) { return x(d[0]) + (x.bandwidth() / 2) })
-      .y(function(d) { return rating(d[4]) })
-      )
+      .x((d) => x(d[0]) + (x.bandwidth() / 2))
+      .y((d) => rating(d[4])))
 
   let max_height = 0;
   side_x.selectAll('.tick').each(function() {
-    if (this.getBBox().height > max_height) 
+    if (this.getBBox().height > max_height)
       max_height = this.getBBox().height;
   })
+
   d3.select("div#side_chart").select('svg')
     .attr("height", side_height + side_margin.top + side_margin.bottom + max_height)
   
@@ -142,12 +141,10 @@ function open_side_window(data) {
       .append("g")
 
   legend.append("rect")
-      .attr("fill", function(d) { return color(d); })
+      .attr("fill", (d) => color(d))
       .attr("width", 15)
       .attr("height", 15)
-      .attr("x", function (d, i) {
-          return i * legspacing - 30;
-      })
+      .attr("x", (_, i) => i * legspacing - 30)
       .attr("y", -30);
 
   legend.append("circle")
@@ -158,14 +155,10 @@ function open_side_window(data) {
 
   legend.append("text")
       .attr("class", "label")
-      .attr("x", function (d, i) {
-          return i * legspacing - 10;
-      })
+      .attr("x", (_, i) => i * legspacing - 10)
       .attr("y", -18)
       .attr("text-anchor", "start")
-      .text(function (d, i) {
-          return legendLabels[i];
-      });
+      .text((_, i) => legendLabels[i]);
 
   legend.append("text")
       .attr("class", "label")
@@ -174,9 +167,10 @@ function open_side_window(data) {
       .attr("text-anchor", "start")
       .text('IMDb Rating');
 
-  side_data.selectAll(".bar-group, .place-label, circle, path").sort(function(d1, d2) {
+  side_data.selectAll(".bar-group, .place-label, circle, path").sort((d1, d2) => {
     if (d1.type === d2.type)
         return 0;
+
     return d1.type === ".bar-group" ? -1 : 1;
   });
 }
